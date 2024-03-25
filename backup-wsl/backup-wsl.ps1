9aafeb30b7e4925d594ec4aed45da05e354f8dc7
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-03-25 15:42:13 -0700

    add wol and updates

--- backup-wsl/backup-wsl.ps1
+++ backup-wsl/backup-wsl.ps1
@@ -4,11 +4,12 @@ $logfile="$HOME\backup-wsl.log.txt"
 if ((Get-Item $logfile).length -gt 128000){
     Remove-Item $logfile
 }
-$src="\\wsl.localhost\Debian\home\tonymet\sotion"
+$src="\\wsl.localhost\Debian\home\${env:Username}\sotion"
 $dest=$(Join-Path ${env:OneDrive} "Documents\Sotion")
 Write-EventLog  -LogName Application -Source "Backup-WSL" -EventID 3001 -Message "Starting Backup"
 $t0=(Get-Date)
-robocopy $src $dest /W:1 /R:0 /E /xd node_modules  /NFL /NDL /LOG+:$logfile
+#robocopy $src $dest /W:1 /R:0 /E /xd node_modules  /NFL /NDL /LOG+:$logfile
+robocopy $src $dest /W:1 /R:0 /E /xd node_modules  /NFL /LOG+:$logfile
 $exitcoderobo=$LASTEXITCODE
 $t1=(Get-Date)
 $delta =($t1-$t0)
---
+++ disable-font/disable.ps1
@@ -0,0 +1,29 @@
+	Function disable-font
+	{
+        
+		[CmdletBinding()]
+		param (
+            [Parameter(Mandatory = $True,
+			   ValueFromPipeline = $True,
+			   ValueFromPipelineByPropertyName = $True,
+			   HelpMessage = 'Name of Font')]
+                [string]$Name
+		)
+		
+		begin
+		{
+			#$sh = New-Object -ComObject "Shell.Application"
+		}
+		
+		process
+		{
+            $sh = New-Object -ComObject "Shell.Application"
+            $sh.Namespace("C:\Windows\Fonts").ParseName($Name) | ForEach-Object { $_.InvokeVerb("hide") }
+            Start-Sleep -Seconds 10
+		}
+		
+		end
+		{
+			
+		}
+	}

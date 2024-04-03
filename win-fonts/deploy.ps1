f097d523d04ea9aae1d9aff3af9004d6d9d1993f
Anthony Metzidis <anthony.metzidis@gmail.com>
2024-04-03 15:04:15 -0700

    remove releasenote

--- win-fonts/deploy.ps1
+++ win-fonts/deploy.ps1
@@ -4,7 +4,6 @@ $parameters = @{
     NuGetApiKey = $secrets.NuGetApiKey
     LicenseUri  = "https://github.com/tonymet/powershell-scripts/blob/master/LICENSE"
     Tag         = "fonts","powershell","powershell-module","PSEdition_Core","Windows"
-    ReleaseNote = "Updated the ActiveDirectory DSC Resources to support adding users."
     Repository  = "PSGallery"
 }
 
